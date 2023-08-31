1c4e07bede9185945785c37e7634fc487d3b7534
unknown <kavi20900@gmail.com>
2023-08-31 15:33:39 +0530

    Product Details

--- src/main/java/com/nt/controller/ProductController.java
+++ src/main/java/com/nt/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.nt.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/product")
 public class ProductController {
 
-	
+	@Value("${dbuser}")
+	private String user;
 	@GetMapping("/show")
 	public ResponseEntity<?> showProduct(){
 		return new ResponseEntity<String>("Product list",HttpStatus.OK);
+	}
+	@GetMapping("/details")
+	public ResponseEntity<?> showDetails(){
+		return new ResponseEntity<String>(user +"Product Details",HttpStatus.OK);
 	}
 }
